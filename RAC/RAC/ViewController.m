8a619c81b393a95a5c44fbb354bc486431c709fb
yan <yan@admindeMacBook-Pro.local>
2018-08-06 19:56:37 +0800

    添加

---
+++ RAC/RAC/ViewController.m
@@ -0,0 +1,56 @@
+//
+//  ViewController.m
+//  RAC
+//
+//  Created by yan on 2018/8/6.
+//  Copyright © 2018年 yan. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ReactiveObjC.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+//    RACSignal *signal = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+//
+//        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//            [subscriber sendNext:@"1"];
+//            [subscriber sendCompleted];
+//        });
+//
+//
+//        return [RACDisposable disposableWithBlock:^{
+//            NSLog(@"结束了");
+//        }];
+//    }];
+//
+//    [signal subscribeNext:^(id  _Nullable x) {
+//        NSLog(@"%@",x);
+//    }];
+    
+    
+    RACSubject *subject = [RACSubject subject];
+    
+    
+    [subject subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@",x);
+    }];
+    
+    [subject sendNext:@"2"];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
